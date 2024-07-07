3d5aaba86e3adfcc6580a9b6cb3fc61aeb8b8e15
Dudune <yahyabakhouchh@gmail.com>
2024-07-07 05:01:48 -0700

    add arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float:
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -12,7 +12,3 @@ def perform_operation(num1: float, num2: float, operation: str) -> float:
             print("cannot devide by zero")
     else:
         print("Invalid operation")
-    
-
-
-
