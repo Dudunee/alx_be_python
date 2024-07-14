cfc0a982b7c360ba6370127349f570003c85bcf5
Dudune <yahyabakhouchh@gmail.com>
2024-07-13 20:12:20 -0700

    add bank_account and main-0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Deposit amount must be bigger than 0")
+    def withdraw(self, amount):
+        if amount > 0 and self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else: 
+            return False
+    
+    def display_balance(self):
+        print(self.account_balance)
